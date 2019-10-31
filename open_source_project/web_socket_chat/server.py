4aefae702c49b0f9c95754cc672e8e97f534723f
cza <chenziangsg@163.com>
2019-10-31 22:15:18 +0800

    update websocket code for learning

---
+++ open_source_project/web_socket_chat/server.py
@@ -0,0 +1,18 @@
+from pywss import Pyws, route, json, ConnectManager
+
+
+@route('/ws/chat')
+def api_news_data(request, data):
+    json_data = json.loads(data)
+    if json_data.get('start') == True:  # 接收start指令
+        # 更新所有已建立连接的socket的当前在线人数
+        request.conn.send_to_all({'online': ConnectManager.online()})
+        return {'sock_id': request.conn.name}  # 返回自身唯一sock_id
+    msg = json_data.get('msg')
+    if msg:  # 获取聊天消息，发送给所有已建立连接的socket
+        request.conn.send_to_all({'from': request.conn.name, 'msg': msg})
+
+
+if __name__ == '__main__':
+    ws = Pyws(__name__, address='127.0.0.1', port=8868)
+    ws.serve_forever()
